c60b0b32f6213e3a1de670db0b4e8ef8e499dff4
askar rostami <askar.rostami@gmail.com>
2021-02-28 07:54:23 +0100

    Auth

---
+++ ParsMarkt/Auth/ApiAuthenticationStateProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ParsMarkt.Auth
+{
+    public class ApiAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonymouse = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymouse)));
+        }
+    }
+}
